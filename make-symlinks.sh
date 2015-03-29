30efc2f3c00ba98e3a8041069ead34409e74d31c
Tyler Thrane <honahu@gmail.com>
2015-03-28 17:36:52 -0700

    Made zshrc not a symlink into zprezto. Removed the XMONAD files.

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -9,7 +9,7 @@
 dir=~/dotfiles # dotfiles directory
 olddir=~/dotfiles_old # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="vimrc vimrc.bundles zshrc oh-my-zsh Xresources gtkwaverc latexmkrc xbindkeysrc Xmodmap fonts"
+files="vimrc vimrc.bundles zshrc Xresources gtkwaverc latexmkrc xbindkeysrc Xmodmap fonts z_sh zprezto zlogin zlogout zshenv zprofile zpreztorc"
 
 ##########
 
@@ -30,31 +30,3 @@ echo "Moving any existing dotfiles from ~ to $olddir"
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
-
-#function install_zsh {
-# Test to see if zshell is installed. If it is:
-#if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-#    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-#git clone http://github.com/robbyrussell/oh-my-zsh.git
-#    fi
-#    # Set the default shell to zsh if it isn't currently set to zsh
-#    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-#chsh -s $(which zsh)
-#    fi
-#else
-    # If zsh isn't installed, get the platform of the current machine
-#    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-#    if [[ $platform == 'Linux' ]]; then
-#sudo apt-get install zsh
-#        install_zsh
-#    # If the platform is OS X, tell the user to install zsh :)
-#    elif [[ $platform == 'Darwin' ]]; then
-#echo "Please install zsh, then re-run this script!"
-#        exit
-#fi
-#fi
-#}
-
-#install_zsh
